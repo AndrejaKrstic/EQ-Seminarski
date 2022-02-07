cff012e17a4e869d8635fe870b377d158410edd4
AndrejaKrstic <andrejakrstic5121@gmail.com>
2022-02-07 20:15:22 +0100

    sredjene rute

--- eq_back/routes/api.php
+++ eq_back/routes/api.php
@@ -13,8 +13,6 @@ Route::get('/words', [WordController::class, 'index'])->name('words.index');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/napredak/{id}', [UserController::class, 'napredak']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
@@ -22,6 +20,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::resource('users', UserController::class)->only(['update', 'store', 'destroy']);
     Route::get('/words/imenice/{id}', [WordController::class, 'imenice']);
     Route::post('/vezbanje', [PractiseController::class, 'store']);
+    Route::get('/napredak/{id}', [UserController::class, 'napredak']);
     Route::post('/users/password', [UserController::class, 'passwordChange']);
     Route::post('/levels', [PractiseController::class, 'getlevels']);
     Route::post('/domaci', [HomeworkController::class, 'addDomaci']);
